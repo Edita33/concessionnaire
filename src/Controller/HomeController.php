0114f4a5e388051a7ff1e9eaa945558ad69e6d4d
Edita Qorraj <e.qorraj@live.com>
2026-05-12 13:09:48 +0200

    Modification de Titre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        dd(bin2hex(random_bytes(16)));
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
