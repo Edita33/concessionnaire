4dcac0285ec36ccfe57f0051b1618ea29a51009f
Edita Qorraj <e.qorraj@live.com>
2026-05-12 13:19:25 +0200

    Modification

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        dd(bin2hex(random_bytes(16)));
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
 }
